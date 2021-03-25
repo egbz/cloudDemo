80cdd551eeb85c30cdc0835e763e80e09fbdbe25
egbz <1023271893@qq.com>
2021-03-25 11:31:16 +0800

    amend

---
+++ cloud-eureka-server7002/src/main/java/org/egbz/clouddemo/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author egbz
+ * @date 2021/3/24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+
+}
