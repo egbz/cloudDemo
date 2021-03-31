21ce3b95490b69508e96b3870a0d53ce955ad35b
egbz <1023271893@qq.com>
2021-03-31 16:27:31 +0800

    amend

---
+++ cloud-consumerzk-order80/src/main/java/org/egbz/clouddemo/OrderZKMain80.java
@@ -0,0 +1,17 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author egbz
+ * @date 2021/3/31
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
