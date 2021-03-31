e992afc8b64773cc6664eb9832dfc9e33a5fadb6
egbz <1023271893@qq.com>
2021-03-31 16:06:57 +0800

    amend

---
+++ cloud-provider-payment8004/src/main/java/org/egbz/clouddemo/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package org.egbz.clouddemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author egbz
+ * @date 2021/3/29
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "spring cloud with zookeeper: " + serverPort + "\t"  + UUID.randomUUID().toString();
+    }
+}
