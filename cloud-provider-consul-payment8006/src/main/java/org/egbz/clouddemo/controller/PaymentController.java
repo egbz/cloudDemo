0ee9e29f10160fc2ada6d60e130f0d540aed0e23
egbz <1023271893@qq.com>
2021-04-01 19:30:45 +0800

    amend

---
+++ cloud-provider-consul-payment8006/src/main/java/org/egbz/clouddemo/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package org.egbz.clouddemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author egbz
+ * @date 2021/3/31
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/consul")
+    public String paymentConsul() {
+        return "springcloud with consul: " + serverPort + "\t" + UUID.randomUUID();
+    }
+
+}
