c465f857af758c9a5fde52facd8c3a1d39b9e91c
egbz <1023271893@qq.com>
2021-04-07 22:27:40 +0800

    amend

---
+++ cloud-config-client-3366/src/main/java/org/egbz/clouddemo/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.io.ObjectInputFilter;
+
+/**
+ * @author egbz
+ * @date 2021/4/7
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
