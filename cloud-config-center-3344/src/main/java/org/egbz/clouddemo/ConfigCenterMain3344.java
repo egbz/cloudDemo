cec02a467170040ad84cbadb6af9ee996ee9ccc3
egbz <1023271893@qq.com>
2021-04-07 17:28:50 +0800

    amend

---
+++ cloud-config-center-3344/src/main/java/org/egbz/clouddemo/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author egbz
+ * @date 2021/4/7
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/org/egbz/clouddemo/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author egbz
+ * @date 2021/4/7
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
