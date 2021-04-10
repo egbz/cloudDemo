51a966d351ae411c583bf31ac15cb399ee16d004
egbz <1023271893@qq.com>
2021-04-10 09:34:33 +0800

    amend

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/org/egbz/clouddemo/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author egbz
+ * @date 2021/4/10
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
