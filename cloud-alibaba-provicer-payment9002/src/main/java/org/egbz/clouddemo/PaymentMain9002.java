21781079dd13d4307154bd7ebb78d8983e299447
egbz <1023271893@qq.com>
2021-04-11 09:44:20 +0800

    amend

---
+++ cloud-alibaba-provicer-payment9002/src/main/java/org/egbz/clouddemo/PaymentMain9002.java
@@ -0,0 +1,17 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author egbz
+ * @date 2021/4/11
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
---
+++ cloud-alibaba-provider-payment9001/src/main/java/org/egbz/clouddemo/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package org.egbz.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import javax.swing.*;
+
+/**
+ * @author egbz
+ * @date 2021/4/11
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
